0dd8780f9cd9fba3c4d6e97abbdd5bde656fbea2
pragati.saxena02 <pragati.saxena02@gmail.com>
2023-05-03 21:20:21 +0530

    EPM Interview

---
+++ EPM.Mouser.Interview.Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using EPM.Mouser.Interview.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace EPM.Mouser.Interview.Web.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        private readonly WarehouseApi warehouseApi;
+        public HomeController(WarehouseApi _warehouseApi)
+        {
+            warehouseApi = _warehouseApi;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            ViewBag.Products = warehouseApi.GetPublicInStockProducts().Value;
+            return View();
+        }
+
+        [HttpGet]
+        [Route("ProductDetail/{id}")]        
+        public IActionResult ProductDetail(long id)
+        {
+
+            ViewBag.Product = warehouseApi.GetProduct(id).Value;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("OrderItem")]
+        public IActionResult OrderAnItem(UpdateQuantityRequest updateQuantityRequest)
+        {
+            ViewBag.response = warehouseApi.OrderItem(updateQuantityRequest);
+            return View();
+        }
+    }
+}
---
+++ EPM.Mouser.Interview.Web/Controllers/ManageProductsController.cs
@@ -0,0 +1,33 @@
+﻿using EPM.Mouser.Interview.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPM.Mouser.Interview.Web.Controllers
+{
+    [Route("/ManageProducts")]
+    public class ManageProductsController : Controller
+    {
+
+        private readonly WarehouseApi warehouseApi;
+        public ManageProductsController(WarehouseApi _warehouseApi)
+        {
+            warehouseApi = _warehouseApi;
+        }
+
+        [HttpGet]
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            ViewBag.result = warehouseApi.AddNewProduct(product).Value;
+            
+            return View();
+        }
+
+       
+
+    }
+}
